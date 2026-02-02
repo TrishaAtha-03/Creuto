289830748743996e41493bdc037b0be2a8844dd6
Trisha Atha <trisha3002@outlook.com>
2026-02-03 01:23:20 +0530

    Updated apiFeatures.js

--- src/utils/apiFeatures.js
+++ src/utils/apiFeatures.js
@@ -4,6 +4,7 @@ class APIFeatures {
     this.queryString = queryString;
   }
 
+  // Text search on name and description
   search() {
     if (this.queryString.search) {
       this.query = this.query.find({
@@ -13,35 +14,50 @@ class APIFeatures {
     return this;
   }
 
+  // Filter by author and publish date range
   filter() {
     const filter = {};
 
+    // Author filter (case-insensitive, exact match, regex-safe)
     if (this.queryString.author) {
-      filter.author = new RegExp(`^${this.queryString.author}$`, "i");
+      const escapedAuthor = this.queryString.author.replace(
+        /[.*+?^${}()|[\]\\]/g,
+        "\\$&"
+      );
+
+      filter.author = new RegExp(`^${escapedAuthor}$`, "i");
     }
 
+    // Publish date range filter
     if (this.queryString.from || this.queryString.to) {
       filter.publishDate = {};
-      if (this.queryString.from)
+
+      if (this.queryString.from) {
         filter.publishDate.$gte = new Date(this.queryString.from);
-      if (this.queryString.to)
+      }
+
+      if (this.queryString.to) {
         filter.publishDate.$lte = new Date(this.queryString.to);
+      }
     }
 
     this.query = this.query.find(filter);
     return this;
   }
 
+  // Sorting
   sort() {
     const sortBy = this.queryString.sortBy || "publishDate";
     const order = this.queryString.order === "asc" ? 1 : -1;
+
     this.query = this.query.sort({ [sortBy]: order });
     return this;
   }
 
+  // Pagination
   paginate() {
-    const page = Math.max(1, parseInt(this.queryString.page) || 1);
-    const limit = Math.min(50, parseInt(this.queryString.limit) || 10);
+    const page = Math.max(1, parseInt(this.queryString.page, 10) || 1);
+    const limit = Math.min(50, parseInt(this.queryString.limit, 10) || 10);
     const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
